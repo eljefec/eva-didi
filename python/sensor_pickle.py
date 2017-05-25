c143105e6aa9a7cc51720690549f5db3af80c984
Jeff <jazzwithjeff@gmail.com>
2017-05-25 02:14:09 -0700

    Prove ability to read ROS msg header from python3.

--- python/sensor_pickle.py
+++ python/sensor_pickle.py
@@ -4,3 +4,6 @@ with open('pointcloud.p', 'rb') as f:
     lidar = pickle.load(f, encoding='latin1')
     print(lidar.shape)
     print(lidar[1:2])
+with open('header.p', 'rb') as f:
+    header = pickle.load(f)
+    print(header)
