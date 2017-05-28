fb8ade97cea9fc22a341d154c9b6a389036079a4
Jeff <writetojeffrey@gmail.com>
2017-05-27 20:41:52 -0700

    Change process_pc2 to PointCloudConverter class.

--- python/lidar.py
+++ python/lidar.py
@@ -10,6 +10,11 @@ import transform_points as tp
 from velodyne_msgs.msg import VelodyneScan
 from sensor_msgs.msg import PointCloud2
 
+import matplotlib
+# Work around error when using matplotlib through ssh: 'Invalid DISPLAY variable'.
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
 def lidar_to_panorama(lidar):
     # These values are for Velodyne HDL-32E.
     return tp.point_cloud_to_panorama(lidar,
@@ -51,58 +56,77 @@ def read_pointclouds(bag_file):
     time.sleep(3)
     return accumulator.msgs
 
-def process_pc2(msg):
-    # CONVERT MESSAGE TO A NUMPY ARRAY OF POINT CLOUDS
-    # creates a Nx5 array: [x, y, z, reflectance, ring]
-    lidar = pc2.read_points(msg)
-    lidar = np.array(list(lidar))
-    print(msg.header.seq)
-    birds_eye = tp.birds_eye_point_cloud(lidar,
-                                         side_range=(-10, 10),
-                                         fwd_range=(-10, 10),
-                                         res=0.1)
-    birds_eye.save('/data/output/birds_eye/' + str(msg.header.seq) + '.png')
+class PointCloudConverter:
+    def __init__(self, savepath, maxcount):
+        if (savepath):
+            folders = ['birds_eye', 'birds_eye2', 'panorama', 'slices']
+            for folder in folders:
+                fullpath = os.path.join(savepath, folder)
+                if not os.path.exists(fullpath):
+                    os.makedirs(fullpath)
+        self.savepath = savepath
+        self.maxcount = maxcount
+        self.count = 0
+        self.lock = threading.Lock()
 
-    birds_eye2 = tp.point_cloud_2_birdseye(lidar,
-                                           res=0.1,
-                                           side_range=(-10, 10),
-                                           fwd_range=(-10, 10),
-                                           height_range=(-2, 2))
+    def on_msg(self, msg):
+        with self.lock:
+            if self.maxcount is None or self.count < self.maxcount:
+                convert = True
+            else:
+                convert = False
+            self.count += 1
+        if convert:
+            # CONVERT MESSAGE TO A NUMPY ARRAY OF POINT CLOUDS
+            # creates a Nx5 array: [x, y, z, reflectance, ring]
+            lidar = pc2.read_points(msg)
+            lidar = np.array(list(lidar))
+            print(msg.header.seq)
+            birds_eye = tp.birds_eye_point_cloud(lidar,
+                                                 side_range=(-10, 10),
+                                                 fwd_range=(-10, 10),
+                                                 res=0.1)
+            birds_eye.save(os.path.join(self.savepath, 'birds_eye/' + str(msg.header.seq) + '.png'))
 
-    imlib.save_np_image(birds_eye2, 'birds_eye2/' + str(msg.header.seq) + '.png')
+            birds_eye2 = tp.point_cloud_2_birdseye(lidar,
+                                                   res=0.1,
+                                                   side_range=(-10, 10),
+                                                   fwd_range=(-10, 10),
+                                                   height_range=(-2, 2))
 
-    # These values are for Velodyne HDL-32E.
-    panorama = tp.point_cloud_to_panorama(lidar,
-                                            v_res = 1.33,
-                                            h_res = 0.4,
-                                            v_fov = (-30.67, 10.67),
-                                            d_range = (0, 100),
-                                            y_fudge = 3)
+            imlib.save_np_image(birds_eye2, os.path.join(self.savepath, 'birds_eye2/' + str(msg.header.seq) + '.png'))
 
-    imlib.save_np_image(panorama, 'panorama/' + str(msg.header.seq) + '.png')
+            # These values are for Velodyne HDL-32E.
+            panorama = tp.point_cloud_to_panorama(lidar,
+                                                    v_res = 1.33,
+                                                    h_res = 0.4,
+                                                    v_fov = (-30.67, 10.67),
+                                                    d_range = (0, 100),
+                                                    y_fudge = 3)
 
-    slices = tp.birds_eye_height_slices(lidar,
-                                        n_slices=8,
-                                        height_range=(-2.0, 0.27),
-                                        side_range=(-10, 10),
-                                        fwd_range=(0, 20),
-                                        res=0.1)
+            imlib.save_np_image(panorama, os.path.join(self.savepath, 'panorama/' + str(msg.header.seq) + '.png'))
 
-    # VISUALISE THE SEPARATE LAYERS IN MATPLOTLIB
-    import matplotlib.pyplot as plt
-    dpi = 100       # Image resolution
-    fig, axes = plt.subplots(2, 4, figsize=(600/dpi, 300/dpi), dpi=dpi)
-    axes = axes.flatten()
-    for i,ax in enumerate(axes):
-        ax.imshow(slices[:,:,i], cmap="gray", vmin=0, vmax=255)
-        ax.set_axis_bgcolor((0, 0, 0))  # Set regions with no points to black
-        ax.xaxis.set_visible(False)     # Do not draw axis tick marks
-        ax.yaxis.set_visible(False)     # Do not draw axis tick marks
-        ax.set_title(i, fontdict={"size": 10, "color":"#FFFFFF"})
-        fig.subplots_adjust(wspace=0.20, hspace=0.20)
+            slices = tp.birds_eye_height_slices(lidar,
+                                                n_slices=8,
+                                                height_range=(-2.0, 0.27),
+                                                side_range=(-10, 10),
+                                                fwd_range=(0, 20),
+                                                res=0.1)
 
-    fig.savefig('/data/output/slices/' + str(msg.header.seq) + '.png')
-    plt.close(fig)
+            # VISUALISE THE SEPARATE LAYERS IN MATPLOTLIB
+            dpi = 100       # Image resolution
+            fig, axes = plt.subplots(2, 4, figsize=(600/dpi, 300/dpi), dpi=dpi)
+            axes = axes.flatten()
+            for i,ax in enumerate(axes):
+                ax.imshow(slices[:,:,i], cmap="gray", vmin=0, vmax=255)
+                ax.set_axis_bgcolor((0, 0, 0))  # Set regions with no points to black
+                ax.xaxis.set_visible(False)     # Do not draw axis tick marks
+                ax.yaxis.set_visible(False)     # Do not draw axis tick marks
+                ax.set_title(i, fontdict={"size": 10, "color":"#FFFFFF"})
+                fig.subplots_adjust(wspace=0.20, hspace=0.20)
+
+            fig.savefig(os.path.join(self.savepath, 'slices/' + str(msg.header.seq) + '.png'))
+            plt.close(fig)
 
 class PointCloudProcessor:
     def __init__(self, hertz):
@@ -209,16 +233,11 @@ if __name__ == '__main__':
     bag_name = '2.bag'
     bag_file = os.path.join(data_dir, bag_name)
 
-    pointclouds = read_pointclouds(bag_file)
-    print('len(pointclouds): {0}'.format(len(pointclouds)))
-
-    exit()
-
-    counter = MessageCounter()
-    pickler = MessagePickler()
-    processor = PointCloudProcessor(40)
-    # processor.add_subscriber(process_pc2)
-    processor.add_subscriber(counter.on_msg)
+    # counter = MessageCounter()
+    # pickler = MessagePickler()
+    converter = PointCloudConverter('/data/output', 1)
+    processor = PointCloudProcessor(hertz = 40)
+    processor.add_subscriber(converter.on_msg)
     # processor.add_subscriber(pickler.on_msg)
 
     # Read rosbag.
