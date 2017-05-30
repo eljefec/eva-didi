da5d98fb5617f44754e0ed0464c9ce66a60d9fe6
Jeff <jazzwithjeff@gmail.com>
2017-05-29 18:58:57 -0700

    Refactor: Extract TrainDataGenerator to its own file.

--- python/model.py
+++ python/model.py
@@ -1,3 +1,4 @@
+from generator import *
 import keras.layers
 from keras.layers.core import Flatten
 from keras.layers import Conv2D, Input, Dense
@@ -5,66 +6,6 @@ from keras.layers.pooling import AveragePooling2D
 from keras.models import Model
 import numpy as np
 import traindata
-
-IMAGE_SHAPE = (1096,1368,3)
-PANORAMA_SHAPE = (38,901,1)
-SLICES_SHAPE = (200,200,8)
-
-INPUT_IMAGE = 'input_image'
-INPUT_LIDAR_PANORAMA = 'input_lidar_panorama'
-INPUT_LIDAR_SLICES = 'input_lidar_slices'
-OUTPUT_POSE = 'output_pose'
-
-class TrainDataGenerator:
-    def __init__(self):
-        self.datastream = traindata.TrainDataStream()
-
-    def generate(self, batch_size, bag_file, tracklet_file):
-        images = []
-        panoramas = []
-        slices_list = []
-        poses = []
-
-        while True:
-            self.datastream.start_read(bag_file, tracklet_file)
-            while not self.datastream.empty():
-                datum = self.datastream.next()
-
-                if datum.lidar_panorama is None:
-                    panorama = np.zeros(PANORAMA_SHAPE, dtype = np.uint8)
-                else:
-                    panorama = np.expand_dims(datum.lidar_panorama, axis=-1)
-
-                if datum.lidar_slices is None:
-                    slices = np.zeros(SLICES_SHAPE, dtype = np.uint8)
-                else:
-                    slices = datum.lidar_slices
-
-                #if np.expand_dims(panorama, axis=-1).shape != PANORAMA_SHAPE:
-                #    print("Panorama shape {0} doesn't match required shape {1}.".format(panorama.shape, PANORAMA_SHAPE))
-                #    continue
-
-                images.append(datum.image)
-                panoramas.append(panorama)
-                slices_list.append(slices)
-                poses.append(datum.pose)
-
-                if batch_size == len(images):
-                    image_batch = np.stack(images)
-                    panorama_batch = np.stack(panoramas)
-                    slices_batch = np.stack(slices_list)
-                    pose_batch = np.stack(poses)
-
-                    images[:] = []
-                    panoramas[:] = []
-                    slices_list[:] = []
-                    poses[:] = []
-
-                    yield ({INPUT_IMAGE: image_batch,
-                            INPUT_LIDAR_PANORAMA: panorama_batch,
-                            INPUT_LIDAR_SLICES: slices_batch},
-                           {OUTPUT_POSE: pose_batch}
-                          )
 
 def train_model():
     input_image = Input(shape = IMAGE_SHAPE,
@@ -114,13 +55,13 @@ def train_model():
     print('model compiled.')
     # model.summary()
 
-    batch_size = 10
+    batch_size = 64
     generator = TrainDataGenerator()
     hist = model.fit_generator(generator.generate(batch_size,
                                                   '/data/Didi-Release-2/Data/1/2.bag',
                                                   '/data/output/test/2/tracklet_labels.xml'),
                                steps_per_epoch = (1584 / batch_size),
-                               epochs = 10)
+                               epochs = 1)
     print(hist)
 
 if __name__ == '__main__':
