d26340c822b68cdbdf990f1dab73284263a8323b
Jeff <writetojeffrey@gmail.com>
2017-06-11 09:46:24 -0700

    Add VelodynePointBagWriter.

--- python/my_bag_utils.py
+++ python/my_bag_utils.py
@@ -2,14 +2,23 @@ import fnmatch
 import os
 import rosbag
 
-def count_image_msgs(bag_file):
+def count_msgs(bag_file, topic):
     bag = rosbag.Bag(bag_file, 'r')
-    count = bag.get_message_count(topic_filters=['/image_raw'])
+    count = bag.get_message_count(topic_filters=[topic])
     bag.close()
 
     return count
 
-def find_bags(directory, pattern):
+def count_image_msgs(bag_file):
+    return count_msgs(bag_file, '/image_raw')
+
+def count_velodyne_packets(bag_file):
+    return count_msgs(bag_file, '/velodyne_packets')
+
+def count_velodyne_points(bag_file):
+    return count_msgs(bag_file, '/velodyne_points')
+
+def find_files(directory, pattern):
     matched_files = []
     for root, dirs, files in os.walk(directory):
         for basename in files:
@@ -18,3 +27,5 @@ def find_bags(directory, pattern):
                 matched_files.append(filename)
     return sorted(matched_files)
 
+def find_bags(directory):
+    return find_files(directory, '*.bag')
