c1470fbc7b29440c3324d65223dd3b6ace0a9e32
Jeff <jazzwithjeff@gmail.com>
2017-06-30 13:08:31 -0700

    Make video of detections.

--- python/predict_tracklet.py
+++ python/predict_tracklet.py
@@ -10,6 +10,7 @@ import sys
 import os
 import glob
 
+import moviepy.editor as mpy
 import numpy as np
 import tensorflow as tf
 
@@ -18,14 +19,12 @@ import numpystream as ns
 import track
 
 from config import *
-# from squeezeDet.src.train import _draw_box
 from nets import *
+import train
 import utils.util
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string(
-    'mode', 'image', """'image' or 'video'.""")
 tf.app.flags.DEFINE_string(
     'checkpoint', '/home/eljefec/repo/squeezeDet/data/model_checkpoints/didi/model.ckpt-6000',
     """Path to the model parameter file.""")
@@ -34,16 +33,23 @@ tf.app.flags.DEFINE_string(
     """Input image or video to be detected. Can process glob input such as """
     """./data/00000*.png.""")
 tf.app.flags.DEFINE_string(
-    'out_dir', './data/out/', """Directory to dump output image or video.""")
+    'out_dir', '/data/out/', """Directory to dump output image or video.""")
 tf.app.flags.DEFINE_string(
     'demo_net', 'didi', """Neural net architecture.""")
 tf.app.flags.DEFINE_string(
     'bag_file', '/data/bags/didi-round2/release/car/training/nissan_driving_past_it/nissan07.bag', """ROS bag.""")
-tf.app.flags.DEFINE_string('gpu', '0', """gpu id.""")
+# tf.app.flags.DEFINE_string('gpu', '0', """gpu id.""")
+tf.app.flags.DEFINE_boolean('make_video', False, """Whether to make video.""")
 
 
-def predict_tracklet(bag_file):
+def predict_tracklet(bag_file, make_video):
   """Detect image."""
+
+  cls2clr = {
+      'car': (255, 191, 0),
+      'cyclist': (0, 191, 255),
+      'pedestrian':(255, 0, 191)
+  }
 
   generator = ns.generate_numpystream(bag_file, tracklet_file = None)
 
@@ -82,6 +88,8 @@ def predict_tracklet(bag_file):
                                 heatmap_threshold_per_frame = 0.5,
                                 vehicle_window_size = 5)
 
+    if make_video:
+      video_maker = VideoMaker()
     frame_idx = 0
 
     with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
@@ -89,6 +97,7 @@ def predict_tracklet(bag_file):
 
       # for f in glob.iglob(FLAGS.input_path):
       for numpydata in generator:
+        frame_idx += 1
         lidar = numpydata.lidar
         if lidar is not None:
           lidar = ld.lidar_to_birdseye(lidar)
@@ -103,8 +112,6 @@ def predict_tracklet(bag_file):
               [model.det_boxes, model.det_probs, model.det_class],
               feed_dict={model.image_input:[input_image]})
 
-          print('det_boxes.shape', det_boxes.shape)
-
           # Filter
           final_boxes, final_probs, final_class = model.filter_prediction(
               det_boxes[0], det_probs[0], det_class[0])
@@ -115,8 +122,14 @@ def predict_tracklet(bag_file):
           final_probs = [final_probs[idx] for idx in keep_idx]
           final_class = [final_class[idx] for idx in keep_idx]
 
-          if final_boxes:
-            print('final_boxes', final_boxes)
+          if make_video:
+            train._draw_box(
+                im, final_boxes,
+                [mc.CLASS_NAMES[idx]+': (%.2f)'% prob \
+                    for idx, prob in zip(final_class, final_probs)],
+                cdict=cls2clr,
+            )
+            video_maker.add_image(im)
 
           car_boxes = []
           car_probs = []
@@ -135,16 +148,44 @@ def predict_tracklet(bag_file):
           car_tracker.track(car_boxes, car_probs)
           ped_tracker.track(ped_boxes, ped_probs)
 
-          print('Frame: {}, Cars: {}, Pedestrians: {}'.format(frame_idx, len(car_tracker.vehicles), len(ped_tracker.vehicles)))
+          print('Frame: {}, Car Boxes: {}, Ped Boxes: {} Tracked Cars: {}, Tracked Peds: {}'.format(frame_idx, len(car_boxes), len(ped_boxes), len(car_tracker.vehicles), len(ped_tracker.vehicles)))
 
-        frame_idx += 1
+  if make_video:
+    video_maker.make_video(filename = 'video_track.mp4')
 
+class VideoMaker:
+  def __init__(self):
+    self.subclip_paths = []
+    self.images = []
+    self.frame_idx = 0
+
+  def add_image(self, image):
+    self.images.append(image)
+    self.frame_idx += 1
+    if self.frame_idx % 500 == 0:
+      self._make_subclip()
+
+  def _make_subclip(self):
+    subclip_path = os.path.join(FLAGS.out_dir, 'subclip_{}.mp4'.format(self.frame_idx))
+    clip = mpy.ImageSequenceClip(self.images, fps=20)
+    clip.write_videofile(subclip_path)
+    self.subclip_paths.append(subclip_path)
+    del self.images[:]
+
+  def make_video(self, filename):
+    self._make_subclip()
+    clips = []
+    for subclip_path in self.subclip_paths:
+      clips.append(mpy.VideoFileClip(subclip_path))
+    final_clip = mpy.concatenate_videoclips(clips)
+    final_clip.write_videofile(os.path.join(FLAGS.out_dir, filename))
+    for subclip_path in self.subclip_paths:
+      os.remove(subclip_path)
 
 def main(argv=None):
   if not tf.gfile.Exists(FLAGS.out_dir):
     tf.gfile.MakeDirs(FLAGS.out_dir)
-  if FLAGS.mode == 'image':
-    predict_tracklet(FLAGS.bag_file)
+  predict_tracklet(FLAGS.bag_file, FLAGS.make_video)
 
 if __name__ == '__main__':
     tf.app.run()
