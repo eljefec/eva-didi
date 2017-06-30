d77278f5c77e2aea4f6d8c69b9c398d4353c1c98
Jeff <writetojeffrey@gmail.com>
2017-06-30 14:21:42 -0700

    Support processing bag folder.

--- python/predict_tracklet.py
+++ python/predict_tracklet.py
@@ -15,8 +15,10 @@ import numpy as np
 import tensorflow as tf
 
 import lidar as ld
+import my_bag_utils as bu
 import numpystream as ns
 import track
+import video
 
 from config import *
 from nets import *
@@ -29,17 +31,15 @@ tf.app.flags.DEFINE_string(
     'checkpoint', '/home/eljefec/repo/squeezeDet/data/model_checkpoints/didi/model.ckpt-6000',
     """Path to the model parameter file.""")
 tf.app.flags.DEFINE_string(
-    'input_path', './data/KITTI/training/image_2/0*0000.png',
-    """Input image or video to be detected. Can process glob input such as """
-    """./data/00000*.png.""")
+    'bag_dir', '/data/bags/didi-round2/release/car/testing', """ROS bag folder""")
 tf.app.flags.DEFINE_string(
     'out_dir', '/data/out/', """Directory to dump output image or video.""")
 tf.app.flags.DEFINE_string(
     'demo_net', 'didi', """Neural net architecture.""")
 tf.app.flags.DEFINE_string(
-    'bag_file', '/data/bags/didi-round2/release/car/training/nissan_driving_past_it/nissan07.bag', """ROS bag.""")
+    'bag_file', '', """ROS bag.""")
 # tf.app.flags.DEFINE_string('gpu', '0', """gpu id.""")
-tf.app.flags.DEFINE_boolean('make_video', False, """Whether to make video.""")
+tf.app.flags.DEFINE_boolean('make_video', True, """Whether to make video.""")
 
 
 def predict_tracklet(bag_file, make_video):
@@ -89,7 +89,7 @@ def predict_tracklet(bag_file, make_video):
                                 vehicle_window_size = 5)
 
     if make_video:
-      video_maker = VideoMaker()
+      video_maker = video.VideoMaker(FLAGS.out_dir)
     frame_idx = 0
 
     with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
@@ -151,41 +151,22 @@ def predict_tracklet(bag_file, make_video):
           print('Frame: {}, Car Boxes: {}, Ped Boxes: {} Tracked Cars: {}, Tracked Peds: {}'.format(frame_idx, len(car_boxes), len(ped_boxes), len(car_tracker.vehicles), len(ped_tracker.vehicles)))
 
   if make_video:
-    video_maker.make_video(filename = 'video_track.mp4')
-
-class VideoMaker:
-  def __init__(self):
-    self.subclip_paths = []
-    self.images = []
-    self.frame_idx = 0
-
-  def add_image(self, image):
-    self.images.append(image)
-    self.frame_idx += 1
-    if self.frame_idx % 500 == 0:
-      self._make_subclip()
-
-  def _make_subclip(self):
-    subclip_path = os.path.join(FLAGS.out_dir, 'subclip_{}.mp4'.format(self.frame_idx))
-    clip = mpy.ImageSequenceClip(self.images, fps=20)
-    clip.write_videofile(subclip_path)
-    self.subclip_paths.append(subclip_path)
-    del self.images[:]
-
-  def make_video(self, filename):
-    self._make_subclip()
-    clips = []
-    for subclip_path in self.subclip_paths:
-      clips.append(mpy.VideoFileClip(subclip_path))
-    final_clip = mpy.concatenate_videoclips(clips)
-    final_clip.write_videofile(os.path.join(FLAGS.out_dir, filename))
-    for subclip_path in self.subclip_paths:
-      os.remove(subclip_path)
+    video_filename = os.path.basename(bag_file) + '.mp4'
+    video_maker.make_video(video_filename)
 
 def main(argv=None):
   if not tf.gfile.Exists(FLAGS.out_dir):
     tf.gfile.MakeDirs(FLAGS.out_dir)
-  predict_tracklet(FLAGS.bag_file, FLAGS.make_video)
+  if FLAGS.bag_file:
+    print('Processing single bag. {}'.format(FLAGS.bag_file))
+    predict_tracklet(FLAGS.bag_file, FLAGS.make_video)
+  elif FLAGS.bag_dir:
+    print('Processing bag folder. {}'.format(FLAGS.bag_dir))
+    bags = bu.find_bags(FLAGS.bag_dir)
+    for bag in bags:
+      predict_tracklet(bag, FLAGS.make_video)
+  else:
+    print('Nothing to do.')
 
 if __name__ == '__main__':
     tf.app.run()
---
+++ python/video.py
@@ -0,0 +1,41 @@
+import moviepy.editor as mpy
+import my_bag_utils as bu
+import os
+
+class VideoMaker:
+  def __init__(self, out_dir):
+    self.out_dir = out_dir
+    self.subclip_paths = []
+    self.images = []
+    self.frame_idx = 0
+
+  def add_image(self, image):
+    self.images.append(image)
+    self.frame_idx += 1
+    if self.frame_idx % 500 == 0:
+      self._make_subclip()
+
+  def _make_subclip(self):
+    subclip_path = os.path.join(self.out_dir, 'subclip_{}.mp4'.format(self.frame_idx))
+    clip = mpy.ImageSequenceClip(self.images, fps=20)
+    clip.write_videofile(subclip_path)
+    self.subclip_paths.append(subclip_path)
+    del self.images[:]
+
+  def make_video(self, filename):
+    self._make_subclip()
+    final_path = os.path.join(self.out_dir, filename)
+    _make_video(self.subclip_paths, final_path)
+
+def _make_video(subclip_paths, filename):
+  clips = []
+  for subclip_path in subclip_paths:
+    clips.append(mpy.VideoFileClip(subclip_path))
+  final_clip = mpy.concatenate_videoclips(clips, method='compose')
+  final_clip.write_videofile(filename)
+  for subclip_path in subclip_paths:
+    os.remove(subclip_path)
+
+if __name__ == '__main__':
+  subclip_paths = bu.find_files('/data/out', 'subclip_*.mp4')
+  _make_video(subclip_paths, 'testing.mp4')
