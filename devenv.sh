4cc3ecce1937315ee3cdcfd58e1b0afa1a54ccf3
Jeff <writetojeffrey@gmail.com>
2017-07-01 16:42:24 -0700

    Add devenv.sh

---
+++ devenv.sh
@@ -0,0 +1,6 @@
+source ~/use_ros.sh
+source ~/use_miniconda2.sh
+# source activate didi2
+source activate squeezeDet
+export PYTHONPATH=$PYTHONPATH:/home/eljefec/repo/squeezeDet/src:/home/eljefec/repo/didi-competition/tracklets/python
+cd ~/repo/eva-didi/python
