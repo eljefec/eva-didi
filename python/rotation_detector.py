dda719e367210b43ad8ebf927d1c74efe67736a5
Jeff <writetojeffrey@gmail.com>
2017-07-02 16:40:15 -0700

    Generate training data for rotation detector.

--- python/rotation_detector.py
+++ python/rotation_detector.py
@@ -1,8 +1,12 @@
+from __future__ import print_function
+
 import crop_images as ci
 import lidar as ld
 import multibag as mb
 import numpy as np
 import numpystream
+import pickle
+import os
 import util.stopwatch
 import cv2
 
@@ -51,40 +55,127 @@ def get_birdseye_box(lidar, obs):
                                     center = (obs.position[0], obs.position[1]))
     return ci.crop_image(birdseye, (51, 51, 3), INPUT_SHAPE)
 
-def generate_birdseye_boxes(multi, batch_size, include_pred):
-    generator = multi.generate(infinite = True)
-    images = []
-    rotations = []
-    count = 0
+def generate_birdseye_boxes_single(multi, infinite):
+    generator = multi.generate(infinite)
     for numpydata in generator:
         lidar = numpydata.lidar
         obs = numpydata.obs[1]
         if lidar is not None:
             birdseye_box = get_birdseye_box(lidar, obs)
 
-            if include_pred(count):
-                images.append(birdseye_box)
-                rotations.append(obs.yaw)
+            yield birdseye_box, obs.yaw
 
-            count += 1
+def makedir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
-            if batch_size == len(images):
-                image_batch = np.stack(images)
-                rotation_batch = np.stack(rotations)
+def get_image_label_dirs(dir):
+    imagedir = os.path.join(dir, 'image')
+    labeldir = os.path.join(dir, 'label')
 
-                images[:] = []
-                rotations[:] = []
+    return (imagedir, labeldir)
 
-                yield (image_batch, rotation_batch)
+def get_image_path(imagedir, id):
+    return os.path.join(imagedir, '{:06d}.png'.format(id))
 
-def train_rotation_detector(multi):
+def get_label_path(labeldir, id):
+    return os.path.join(labeldir, '{:06d}.txt'.format(id))
+
+def generate_training_data(multi, outdir):
+    imagedir, labeldir = get_image_label_dirs(outdir)
+
+    makedir(imagedir)
+    makedir(labeldir)
+
+    id = 0
+    generator = generate_birdseye_boxes_single(multi, infinite = False)
+    for birdseye_box, yaw in generator:
+        image_path = get_image_path(imagedir, id)
+        cv2.imwrite(image_path, birdseye_box)
+
+        label_path = get_label_path(labeldir, id)
+        with open(label_path, 'w') as f:
+            print(yaw, file=f)
+
+        id += 1
+
+    idx = []
+    with open(os.path.join(outdir, 'trainval.txt'), 'w') as f:
+        for i in range(id):
+            formatted = '{:06d}'.format(i)
+            print(formatted, file=f)
+            idx.append(formatted)
+
+    train_file = os.path.join(outdir, 'train.txt')
+    val_file = os.path.join(outdir, 'val.txt')
+
+    idx = np.random.permutation(idx)
+
+    val_split = len(idx) / 4
+    val_idx = sorted(idx[val_split:])
+    train_idx = sorted(idx[:val_split])
+
+    with open(train_file, 'w') as f:
+        for i in train_idx:
+            print(i, file=f)
+
+    with open(val_file, 'w') as f:
+        for i in val_idx:
+            print(i, file=f)
+
+    print('Trainining set is saved to ' + train_file)
+    print('Validation set is saved to ' + val_file)
+
+def get_size(train_dir, index_file):
+    fullpath = os.path.join(train_dir, index_file)
+    ids = []
+    with open(fullpath, 'r') as f:
+        for id in f:
+            ids.append(id)
+    return len(ids)
+
+def generate_birdseye_boxes_dir(train_dir, index_file, infinite = True):
+    while infinite:
+        ids = []
+        with open(os.path.join(train_dir, index_file), 'r') as f:
+            for id in f:
+                ids.append(int(id))
+
+        imagedir, labeldir = get_image_label_dirs(train_dir)
+
+        for id in ids:
+            image_path = get_image_path(imagedir, id)
+            birdseye_box = cv2.imread(image_path)
+
+            label_path = get_label_path(labeldir, id)
+            with open(label_path, 'r') as f:
+                yaw = float(f.readline())
+
+            yield birdseye_box, yaw
+
+def generate_birdseye_boxes(single_generator, batch_size):
+    images = []
+    yaws = []
+    for birdseye_box, yaw in single_generator:
+        images.append(birdseye_box)
+        yaws.append(yaw)
+
+        if batch_size == len(images):
+            image_batch = np.stack(images)
+            yaw_batch = np.stack(yaws)
+
+            images[:] = []
+            yaws[:] = []
+
+            yield (image_batch, yaw_batch)
+
+def train_rotation_detector(train_dir):
     batch_size = 128
 
-    pred_validation = lambda count : count % 5 == 0
-    pred_train = lambda count : count % 5 != 0
-
-    generator_validation = generate_birdseye_boxes(multi, batch_size, pred_validation)
-    generator_train = generate_birdseye_boxes(multi, batch_size, pred_train)
+    generator_validation = generate_birdseye_boxes(
+                                generate_birdseye_boxes_dir(train_dir, 'val.txt'), batch_size)
+    generator_train = generate_birdseye_boxes(
+                                generate_birdseye_boxes_dir(train_dir, 'train.txt'), batch_size)
 
     checkpoint_path = get_model_filename(CHECKPOINT_DIR, suffix = 'e{epoch:02d}-vl{val_loss:.2f}')
 
@@ -105,14 +196,17 @@ def train_rotation_detector(multi):
 
     model.compile(optimizer = Adam(lr = 0.0001), loss = 'mse', metrics = ['accuracy'])
 
+    steps_per_epoch = get_size(train_dir, 'train.txt') / batch_size
+    validation_steps = get_size(train_dir, 'val.txt') / batch_size
+
     hist = model.fit_generator(generator_train,
-                               steps_per_epoch = int(0.8 * (multi.count() / batch_size)),
+                               steps_per_epoch = steps_per_epoch,
                                epochs = 100,
                                # Values for quick testing:
                                # steps_per_epoch = (128 / batch_size),
                                # epochs = 2,
                                validation_data = generator_validation,
-                               validation_steps = int(0.2 * (multi.count() / batch_size)),
+                               validation_steps = validation_steps,
                                callbacks = callbacks)
     model.save(get_model_filename(MODEL_DIR))
 
@@ -142,4 +236,7 @@ if __name__ == '__main__':
 
     multi = mb.MultiBagStream(bt, numpystream.generate_numpystream)
 
-    train_rotation_detector(multi)
+    train_dir = '/data/rot_train'
+    generate_training_data(multi, train_dir)
+
+    train_rotation_detector(train_dir)
