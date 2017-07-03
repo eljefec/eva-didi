55fc67337d27e84c05f0a52d30e1473ca346ea81
Jeff <writetojeffrey@gmail.com>
2017-07-03 01:35:14 -0700

    Project 3D global point onto 2D camera image.

--- python/camera_detector.py
+++ python/camera_detector.py
@@ -12,16 +12,27 @@ def read_ost_yaml():
 
 def read_ost_array(ost, field):
     ost_cm = ost[field]
-    return np.array(ost_cm['data']).reshape(ost_cm['cols'], ost_cm['rows'])
+    return np.array(ost_cm['data']).reshape(ost_cm['rows'], ost_cm['cols'])
 
 class Undistorter:
     def __init__(self):
         ost = read_ost_yaml()
         self.camera_matrix = read_ost_array(ost, 'camera_matrix')
         self.distortion_coefficients = read_ost_array(ost, 'distortion_coefficients')
+        self.projection_matrix = read_ost_array(ost, 'projection_matrix')
 
     def undistort_image(self, im):
         return cv2.undistort(im, self.camera_matrix, self.distortion_coefficients)
+
+    def project_point(self, object_point):
+        # See formulas at http://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html
+        z_camera = object_point[0]
+        x_camera = -object_point[1]
+        y_camera = -object_point[2]
+        camera_coord = np.array([x_camera, y_camera, z_camera, 1.0]).transpose()
+        image_point = np.dot(self.projection_matrix, camera_coord)
+        image_point /= image_point[2]
+        return image_point
 
 def try_undistort(desired_count):
     undist = Undistorter()
@@ -34,9 +45,14 @@ def try_undistort(desired_count):
     output_count = 0
     for numpydata in generator:
         im = numpydata.image
+        frame_idx, obs = numpydata.obs
         undistorted = undist.undistort_image(im)
         if count % 25 == 0:
             cv2.imwrite('/data/dev/orig_{}.png'.format(count), im)
+
+            img_point = undist.project_point(obs.position)
+            print('img_point', img_point)
+            cv2.circle(undistorted, (int(img_point[0]), int(img_point[1])), radius = 5, color = (255, 0, 0), thickness=2)
             cv2.imwrite('/data/dev/undist_{}.png'.format(count), undistorted)
             output_count += 1
         count += 1
