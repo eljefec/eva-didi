990b6af9a804c152cc2e5b070b2f9dc04bb95231
Jeff <jazzwithjeff@gmail.com>
2017-07-03 19:24:40 -0700

    Fix new pipeline. Output matches old pipeline for birdseye.

--- python/detection_pipeline.py
+++ python/detection_pipeline.py
@@ -1,10 +1,12 @@
-import squeezedet as sd
+import numpy as np
+
+import birdseye_detector as bd
 import camera_detector as cd
 
 class DetectionPipeline:
     def __init__(self, enable_birdseye, enable_camera):
         if enable_birdseye:
-            self.birdseye_detector = sd.BirdsEyeDetector()
+            self.birdseye_detector = bd.BirdsEyeDetector()
         else:
             self.birdseye_detector = None
 
@@ -13,28 +15,28 @@ class DetectionPipeline:
         else:
             self.camera_detector = None
 
-        self.prev_car = None
-        self.prev_ped = None
+        self.prev_car = np.array([0., 0., -0.9, 0.])
+        self.prev_ped = np.array([0., 0., -0.9, 0.])
         self.prev_t = None
 
-    def detect(self, lidar, t):
+    def detect_lidar(self, lidar, t):
         if self.birdseye_detector is not None:
             car, ped = self.birdseye_detector.detect(lidar)
-            add_detection(car, ped, t)
+            self._add_detection(car, ped, t)
 
-    def detect(self, image, t):
+    def detect_image(self, image, t):
         if self.camera_detector is not None:
             car, ped = self.camera_detector.detect(image)
-            add_detection(car, ped, t)
+            self._add_detection(car, ped, t)
 
-    def add_detection(car, ped, t):
+    def _add_detection(self, car, ped, t):
         if self.prev_t is None:
             self.prev_t = t
         else:
             dt = t - self.prev_t
 
         self.prev_car = car
-        self.prev_ped = pred
+        self.prev_ped = ped
 
     def estimate_positions(self):
         return self.prev_car, self.prev_ped
