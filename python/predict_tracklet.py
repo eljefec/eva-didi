e436f94bc46054e8f26596cf13ba2f30ae5e51c8
Jeff <writetojeffrey@gmail.com>
2017-07-03 19:24:40 -0700

    Fix new pipeline. Output matches old pipeline for birdseye.

--- python/predict_tracklet.py
+++ python/predict_tracklet.py
@@ -1,8 +1,13 @@
+import numpy as np
+import os
+import sensor_msgs.point_cloud2 as pc2
+import tensorflow as tf
+
 import detection_pipeline as dp
+import image
 import generate_tracklet
 import my_bag_utils as bu
 import sensor
-import sensor_msgs.point_cloud2 as pc2
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -14,25 +19,27 @@ tf.app.flags.DEFINE_string(
     'bag_file', '', """ROS bag.""")
 tf.app.flags.DEFINE_boolean('include_car', False, """Whether to include car in tracklet.""")
 tf.app.flags.DEFINE_boolean('include_ped', False, """Whether to include pedestrian in tracklet.""")
+tf.app.flags.DEFINE_boolean('enable_birdseye', False, """Whether to enable birdseye detection.""")
+tf.app.flags.DEFINE_boolean('enable_camera', False, """Whether to enable camera detection.""")
 
 def generate_predictions(pipeline, bag_file):
     generator = sensor.generate_sensormsgs(bag_file)
     for msg in generator:
         t_seconds = msg.header.stamp.to_sec()
         if msg.header.frame_id == 'camera':
-            imagemsg = image.ImageMsg(frame.image)
-            pipeline.detect(imagemsg.bgr, t_seconds)
+            imagemsg = image.ImageMsg(msg)
+            pipeline.detect_image(imagemsg.bgr, t_seconds)
 
             car, ped = pipeline.estimate_positions()
             yield car, ped
 
         elif msg.header.frame_id == 'velodyne':
-            points = pc2.read_points(frame.lidar)
+            points = pc2.read_points(msg)
             points = np.array(list(points))
-            pipeline.detect(points, t_seconds)
+            pipeline.detect_lidar(points, t_seconds)
 
-def predict_tracklet(bag_file, include_car, include_ped):
-    pipeline = dp.DetectionPipeline()
+def predict_tracklet(bag_file, include_car, include_ped, enable_birdseye, enable_camera):
+    pipeline = dp.DetectionPipeline(enable_birdseye, enable_camera)
 
     def make_pose(x, y, rz):
       # Estimate tz from histogram.
@@ -55,10 +62,14 @@ def predict_tracklet(bag_file, include_car, include_ped):
 
     for car, ped in generator:
         # car is tx, ty, tz, rz
-        car_pose = make_pose(car[0], car[1], car[3])
-        car_tracklet.poses.append(pose)
+        if car is not None:
+            car_pose = make_pose(car[0], car[1], car[3])
+            prev_car_pose = car_pose
+        car_tracklet.poses.append(car_pose)
 
-        ped_pose = make_pose(ped[0], ped[1], ped[3])
+        if ped is not None:
+            ped_pose = make_pose(ped[0], ped[1], ped[3])
+            prev_ped_pose = ped_pose
         ped_tracklet.poses.append(ped_pose)
 
     tracklet_collection = generate_tracklet.TrackletCollection()
@@ -71,14 +82,25 @@ def predict_tracklet(bag_file, include_car, include_ped):
 
     tracklet_collection.write_xml(tracklet_file)
 
+def get_filename(bag_file):
+    base = os.path.basename(bag_file)
+    split = os.path.splitext(base)
+    return split[0]
+
 def process_bag(bag_file):
     print('Generate tracklet')
     print('Include car: ', FLAGS.include_car)
     print('Include ped: ', FLAGS.include_ped)
+    print('Enable birdseye: ', FLAGS.enable_birdseye)
+    print('Enable camera: ', FLAGS.enable_camera)
     if not FLAGS.include_car and not FLAGS.include_ped:
         print('Must include one of the obstacle types.')
         exit()
-    predict_tracklet(bag_file, FLAGS.include_car, FLAGS.include_ped)
+    if not FLAGS.enable_birdseye and not FLAGS.enable_camera:
+        print('Must enable one or more of the detectors.')
+        exit()
+
+    predict_tracklet(bag_file, FLAGS.include_car, FLAGS.include_ped, FLAGS.enable_birdseye, FLAGS.enable_camera)
 
 def main(argv=None):
     if not tf.gfile.Exists(FLAGS.out_dir):
