becaa19bf55827ae60774056eb6dc5c4085989ee
Jeff <writetojeffrey@gmail.com>
2017-07-05 02:30:04 -0700

    Generate panorama train data resized for squeezeDet.

--- python/panorama_detector.py
+++ python/panorama_detector.py
@@ -26,6 +26,13 @@ def generate_panoramas(numpydata_generator):
                              [np.max(bbox[0]), np.max(bbox[1])]
                             ])
 
+            assert(im.shape == (38, 901, 3)), 'Shape was {}'.format(im.shape)
+
+            # Resize for squeezeDet, which requires dimensions that are multiples of 16.
+            im = cv2.resize(im, (912, 48))
+
+            assert(im.shape == (48, 912, 3)), 'Shape was {}'.format(im.shape)
+
             yield (im, bbox, obs)
 
 def generate_panoramas_multi(multi):
@@ -36,8 +43,8 @@ def makedir(dir):
         os.makedirs(dir)
 
 def get_bbox_label_dirs(dir):
-    imagedir = os.path.join(dir, 'image')
-    labeldir = os.path.join(dir, 'label')
+    imagedir = os.path.join(dir, 'image_2')
+    labeldir = os.path.join(dir, 'label_2')
 
     return (labeldir, imagedir)
 
@@ -64,28 +71,34 @@ def write_train_data(multi, outdir):
         if id % 1000 == 0:
             print('Wrote {} examples.'.format(id))
 
+    util.traingen.write_train_val(outdir, id)
+
 def try_write():
     bagdir = '/data/bags/'
+    # bagdir = '/data/bags/didi-round2/release/car/training/suburu_leading_front_left'
     bt = mb.find_bag_tracklets(bagdir, '/data/tracklets')
     multi = mb.MultiBagStream(bt, ns.generate_numpystream)
-    write_train_data(multi, '/home/eljefec/repo/squeezeDet/data/KITTI/panorama_train')
+    write_train_data(multi, '/home/eljefec/repo/squeezeDet/data/KITTI/panorama912x48')
 
 def try_draw_panoramas():
     import cv2
     import matplotlib.pyplot as plt
     import matplotlib.image as mpimg
-    imagedir = '/data/dev/panorama_train'
-    bagdir = '/data/bags/didi-round2/release/car/training/suburu_leading_at_distance'
+    # bagdir = '/data/bags/didi-round2/release/car/training/suburu_leading_front_left'
+    bagdir = '/data/bags/didi-round2/release/pedestrian/'
+    # bag_file = '/data/bags/didi-round2/release/car/testing/ford02.bag'
     bt = mb.find_bag_tracklets(bagdir, '/data/tracklets')
     multi = mb.MultiBagStream(bt, ns.generate_numpystream)
-    generator = generate_panoramas(multi.generate(infinite = False))
+
+    # numpystream = ns.generate_numpystream(bag_file, tracklet)
+    generator = generate_panoramas_multi(multi)
 
     id = 1
-    for im, bbox in generator:
+    for im, bbox, obs in generator:
         print('bbox', bbox)
-        # image_file = os.path.join(imagedir, '{:06d}.png'.format(id))
-        # imlib.save_np_image(im, image_file, bbox)
-        cv2.rectangle(im, bbox[0], bbox[1], color = (255, 0, 0))
+        print('bbox[0]', bbox[0])
+        cv2.rectangle(im, tuple(bbox[0]), tuple(bbox[1]), color = (255, 0, 0))
+        im = cv2.resize(im, (0,0), fx=1.0, fy=8.0)
         plt.imshow(im)
         plt.show()
 
